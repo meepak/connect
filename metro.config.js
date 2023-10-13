c10fb9dcd1fd6b4147ea7520f579d5f8d31881a9
Deepak Mahat <meepak@gmail.com>
2023-10-13 19:11:07 +1030

    metro config removed

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const { getDefaultConfig } = require('@expo/metro-config')
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
 
-const defaultConfig = getDefaultConfig(__dirname)
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
 
-defaultConfig.resolver.assetExts.push('cjs')
-
-module.exports = defaultConfig
+module.exports = config
